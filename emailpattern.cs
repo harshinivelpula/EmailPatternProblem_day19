96c9b25bd3f1f97d049c5a8c9836d6a4fca7e31a
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-12-17 14:26:09 +0530

    simple email pattern

---
+++ emailpattern.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace EmailPatternProblem
+{
+    public class emailpattern
+    {
+        public static string regexrule = "^[a-z]{3,8}[@]{1}[a-z]{3,8}[.]{1}[a-z]{3}";
+        public bool validemail(string validemail)
+        {
+            return Regex.IsMatch(validemail, regexrule);
+        }
+    }
+}
