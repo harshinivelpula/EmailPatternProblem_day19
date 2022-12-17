8583da1ce6f688fb3d5ba156bc8cf2c9cfb0bed3
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-12-17 14:25:15 +0530

    simple email pattern

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace EmailPatternProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            emailpattern emailPattern = new emailpattern();
+            Console.WriteLine("enter the email");
+            string email = Console.ReadLine();
+            bool value = emailPattern.validemail(email);
+            if (value)
+            {
+                Console.WriteLine("matched");
+            }
+            else
+            {
+                Console.WriteLine("not matched");
+            }
+        }
+    }
+}
